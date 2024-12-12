1a0b3d564a14bade5df0e40e413c3ffdc6fba03a
Fernando Palacios <fernandopalacios@Fernandos-MacBook-Pro.local>
2024-12-11 19:31:44 -0600

    Created the Authorization service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { isDevMode } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  apiUrl: string = isDevMode() ? 'http://localhost:5218/api/access' : 'http://f2.barralibre.io/api/access';
+
+  constructor(private httpClient: HttpClient, private router: Router) { }
+
+  login(payload: any) {
+    return this.httpClient.post<any>(`${this.apiUrl}/login`, payload);
+  }
+  
+  setSession(user: string, token: string) {
+    sessionStorage.setItem('user', user); 
+    sessionStorage.setItem('token', token); 
+  }
+
+  logout() {
+    sessionStorage.removeItem('user');
+    sessionStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+}
